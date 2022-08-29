98c3bb9cb6ec6df9b6c24ee4c911c87e243ac6a7
SaadDamine <m.sdaminesaad@gmail.com>
2022-08-29 18:56:11 +0100

    Set Up Router and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { HomePage } from './pages/homepage/HomePage.component';
 
+import { Routes,Route } from 'react-router-dom';
+
+
+const Test = () => (
+  <div>this is Test page</div>
+);
+
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Routes>
+        <Route  path='/' element={<HomePage />} />
+        <Route  path='/test' element={<Test />} />
+      </Routes>
     </div>
   );
 }
