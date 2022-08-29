5ab493c71cff9773ca4ad56f0b03a5faa2dfbf36
SaadDamine <m.sdaminesaad@gmail.com>
2022-08-29 20:40:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { HomePage } from './pages/homepage/HomePage.component';
+import HomePage from './pages/homepage/HomePage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import { Routes,Route } from 'react-router-dom';
 
@@ -12,7 +13,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route  path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
         <Route path='/hats' element={<Hats />} />
       </Routes>
     </div>
