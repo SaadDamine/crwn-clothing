30cde9717b8b7d60a063041fee3e62dd6980d11a
SaadDamine <m.sdaminesaad@gmail.com>
2022-08-31 13:39:38 +0100

    add Sign in Component to Entry

--- src/pages/entry/entry.component.jsx
+++ src/pages/entry/entry.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
 
 import './entry.styles.scss'
 
 const Entry = () => (
     <div>
-        Entry Page
+        <SignIn/>
     </div>
 );
 
